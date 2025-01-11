bd5aee9b6a7fdb1d9546e7c57606b1be6bc2b099
LolgamerHD <dahlemlinus@gmail.com>
2025-01-11 20:25:39 +0100

    Added New Player Movement and Added Gun

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Gun : MonoBehaviour
+{
+    public UnityEvent OnGunShoot;
+    public float FireCooldown;
+
+    // By default gun is semi
+    public bool Automatic;
+
+    private float CurrentCooldown;
+    void Start()
+    {
+        CurrentCooldown = FireCooldown;
+    }
+
+    void Update()
+    {
+        if (Automatic)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (CurrentCooldown <= 0f)
+                {
+                    OnGunShoot.Invoke();
+                    CurrentCooldown = FireCooldown;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (CurrentCooldown <= 0f)
+                {
+                    OnGunShoot.Invoke();
+                    CurrentCooldown = FireCooldown;
+                }
+            }
+        }
+
+        CurrentCooldown -= Time.deltaTime;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,290 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Assignables
-    public Transform playerCam;
-    public Transform orientation;
+    public float MoveSmoothTime;
+    public float GravityStrength;
+    public float JumpStrength;
+    public float WalkSpeed;
+    public float RunSpeed;
 
-    //Other
-    private Rigidbody rb;
-    private Animator animator;
+    private CharacterController Controller;
+    private Vector3 CurrentMoveVelocity;
+    private Vector3 MoveDampVelocity;
 
-    // Animation parameters
-    private static readonly int SpeedParam = Animator.StringToHash("Speed");
-    private static readonly int IsJumpingParam = Animator.StringToHash("IsJumping");
-    private static readonly int IsCrouchingParam = Animator.StringToHash("IsCrouching");
-    private static readonly int IsWalkingParam = Animator.StringToHash("IsWalking");
-
-    // Animation smoothing
-    private float velocitySmoothing = 0.1f;
-    private float currentSpeed;
-
-    //Rotation and look
-    private float xRotation;
-    private float sensitivity = 50f;
-    private float sensMultiplier = 1f;
-
-    //Movement
-    public float moveSpeed = 4500;
-    public float maxSpeed = 20;
-    public bool grounded;
-    public LayerMask whatIsGround;
-
-    public float counterMovement = 0.175f;
-    private float threshold = 0.01f;
-    public float maxSlopeAngle = 35f;
-
-    //Crouch & Slide
-    private Vector3 crouchScale = new Vector3(1, 0.5f, 1);
-    private Vector3 playerScale;
-    public float slideForce = 400;
-    public float slideCounterMovement = 0.2f;
-
-    //Jumping
-    private bool readyToJump = true;
-    private float jumpCooldown = 0.25f;
-    public float jumpForce = 550f;
-
-    //Input
-    float x, y;
-    bool jumping, sprinting, crouching;
-
-    //Sliding
-    private Vector3 normalVector = Vector3.up;
-    private Vector3 wallNormalVector;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>();
-    }
+    private Vector3 CurrentForceVelocity;
 
     void Start()
     {
-        playerScale = transform.localScale;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Controller = GetComponent<CharacterController>();
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        Movement();
-    }
-
-    private void Update()
-    {
-        MyInput();
-        Look();
-        UpdateAnimations();
-    }
-
-    private void UpdateAnimations()
-    {
-        Vector3 horizontalVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-        float targetSpeed = horizontalVelocity.magnitude;
-
-        if (targetSpeed < 0.01f)
-            targetSpeed = 0f;
-
-        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, velocitySmoothing);
-
-        animator.SetFloat(SpeedParam, currentSpeed);
-        animator.SetBool(IsJumpingParam, !grounded);
-        animator.SetBool(IsCrouchingParam, crouching);
-
-        animator.SetBool(IsWalkingParam, currentSpeed > 0.1f);
-    }
-
-    private void MyInput()
-    {
-        x = Input.GetAxisRaw("Horizontal");
-        y = Input.GetAxisRaw("Vertical");
-        jumping = Input.GetButton("Jump");
-        crouching = Input.GetKey(KeyCode.LeftControl);
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-            StartCrouch();
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-            StopCrouch();
-    }
-
-    private void StartCrouch()
-    {
-        transform.localScale = crouchScale;
-        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
-        animator.SetBool(IsCrouchingParam, true);
-
-        if (rb.linearVelocity.magnitude > 0.5f && grounded)
+        Vector3 PlayerInput = new Vector3
         {
-            rb.AddForce(orientation.transform.forward * slideForce);
-        }
-    }
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical")
+        };
 
-    private void StopCrouch()
-    {
-        transform.localScale = playerScale;
-        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
-        animator.SetBool(IsCrouchingParam, false);
-    }
-
-    private void Movement()
-    {
-        rb.AddForce(Vector3.down * Time.deltaTime * 10);
-
-        Vector2 mag = FindVelRelativeToLook();
-        float xMag = mag.x, yMag = mag.y;
-
-        CounterMovement(x, y, mag);
-
-        if (readyToJump && jumping) Jump();
-
-        float maxSpeed = this.maxSpeed;
-
-        if (crouching && grounded && readyToJump)
+        if (PlayerInput.magnitude > 1f)
         {
-            rb.AddForce(Vector3.down * Time.deltaTime * 3000);
-            return;
+            PlayerInput.Normalize();
         }
 
-        if (x > 0 && xMag > maxSpeed) x = 0;
-        if (x < 0 && xMag < -maxSpeed) x = 0;
-        if (y > 0 && yMag > maxSpeed) y = 0;
-        if (y < 0 && yMag < -maxSpeed) y = 0;
+        Vector3 MoveVector = transform.TransformDirection(PlayerInput);
+        float CurrentSpeed = Input.GetKey(KeyCode.LeftShift) ? RunSpeed : WalkSpeed;
 
-        float multiplier = 1f, multiplierV = 1f;
+        CurrentMoveVelocity = Vector3.SmoothDamp(
+            CurrentMoveVelocity,
+            MoveVector * CurrentSpeed,
+            ref MoveDampVelocity,
+            MoveSmoothTime
+        );
 
-        if (!grounded)
+        Controller.Move(CurrentMoveVelocity * Time.deltaTime);
+
+        Ray groundCheckRay = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(groundCheckRay, 1.1f))
         {
-            multiplier = 0.5f;
-            multiplierV = 0.5f;
-        }
+            CurrentForceVelocity.y = -2f;
 
-        if (grounded && crouching) multiplierV = 0f;
-
-        rb.AddForce(orientation.transform.forward * y * moveSpeed * Time.deltaTime * multiplier * multiplierV);
-        rb.AddForce(orientation.transform.right * x * moveSpeed * Time.deltaTime * multiplier);
-    }
-
-    private void Jump()
-    {
-        if (grounded && readyToJump)
-        {
-            readyToJump = false;
-            animator.SetBool(IsJumpingParam, true);
-
-            rb.AddForce(Vector2.up * jumpForce * 1.5f);
-            rb.AddForce(normalVector * jumpForce * 0.5f);
-
-            Vector3 vel = rb.linearVelocity;
-            if (rb.linearVelocity.y < 0.5f)
-                rb.linearVelocity = new Vector3(vel.x, 0, vel.z);
-            else if (rb.linearVelocity.y > 0)
-                rb.linearVelocity = new Vector3(vel.x, vel.y / 2, vel.z);
-
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
-    }
-
-    private void ResetJump()
-    {
-        readyToJump = true;
-    }
-
-    private float desiredX;
-    private void Look()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
-
-        Vector3 rot = playerCam.transform.localRotation.eulerAngles;
-        desiredX = rot.y + mouseX;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
-        orientation.transform.localRotation = Quaternion.Euler(0, desiredX, 0);
-    }
-
-    private void CounterMovement(float x, float y, Vector2 mag)
-    {
-        if (!grounded || jumping) return;
-
-        if (crouching)
-        {
-            rb.AddForce(moveSpeed * Time.deltaTime * -rb.linearVelocity.normalized * slideCounterMovement);
-            return;
-        }
-
-        if (Math.Abs(mag.x) > threshold && Math.Abs(x) < 0.05f || (mag.x < -threshold && x > 0) || (mag.x > threshold && x < 0))
-        {
-            rb.AddForce(moveSpeed * orientation.transform.right * Time.deltaTime * -mag.x * counterMovement);
-        }
-        if (Math.Abs(mag.y) > threshold && Math.Abs(y) < 0.05f || (mag.y < -threshold && y > 0) || (mag.y > threshold && y < 0))
-        {
-            rb.AddForce(moveSpeed * orientation.transform.forward * Time.deltaTime * -mag.y * counterMovement);
-        }
-
-        if (Mathf.Sqrt((Mathf.Pow(rb.linearVelocity.x, 2) + Mathf.Pow(rb.linearVelocity.z, 2))) > maxSpeed)
-        {
-            float fallspeed = rb.linearVelocity.y;
-            Vector3 n = rb.linearVelocity.normalized * maxSpeed;
-            rb.linearVelocity = new Vector3(n.x, fallspeed, n.z);
-        }
-    }
-
-    public Vector2 FindVelRelativeToLook()
-    {
-        float lookAngle = orientation.transform.eulerAngles.y;
-        float moveAngle = Mathf.Atan2(rb.linearVelocity.x, rb.linearVelocity.z) * Mathf.Rad2Deg;
-
-        float u = Mathf.DeltaAngle(lookAngle, moveAngle);
-        float v = 90 - u;
-
-        float magnitude = rb.linearVelocity.magnitude;
-        float yMag = magnitude * Mathf.Cos(u * Mathf.Deg2Rad);
-        float xMag = magnitude * Mathf.Cos(v * Mathf.Deg2Rad);
-
-        return new Vector2(xMag, yMag);
-    }
-
-    private bool IsFloor(Vector3 v)
-    {
-        float angle = Vector3.Angle(Vector3.up, v);
-        return angle < maxSlopeAngle;
-    }
-
-    private bool cancellingGrounded;
-
-    private void OnCollisionStay(Collision other)
-    {
-        int layer = other.gameObject.layer;
-        if (whatIsGround != (whatIsGround | (1 << layer))) return;
-
-        for (int i = 0; i < other.contactCount; i++)
-        {
-            Vector3 normal = other.contacts[i].normal;
-            if (IsFloor(normal))
+            if (Input.GetKey(KeyCode.Space))
             {
-                grounded = true;
-                cancellingGrounded = false;
-                normalVector = normal;
-                CancelInvoke(nameof(StopGrounded));
+                CurrentForceVelocity.y = JumpStrength;
             }
         }
-
-        float delay = 3f;
-        if (!cancellingGrounded)
+        else
         {
-            cancellingGrounded = true;
-            Invoke(nameof(StopGrounded), Time.deltaTime * delay);
+            CurrentForceVelocity.y = GravityStrength * Time.deltaTime;
         }
-    }
 
-    private void StopGrounded()
-    {
-        grounded = false;
+        Controller.Move(CurrentForceVelocity * Time.deltaTime);
     }
 }
